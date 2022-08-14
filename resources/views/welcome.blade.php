397e10418bda44a79ad606f815af1a5eff8e32db
pangdaren <102470557+darenpanda@users.noreply.github.com>
2022-08-14 09:03:20 +0900

    NPM実施

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>パン屋さんの業務アプリ</title>
-        パン屋さんの業務
+        パン屋さんの業務（卒業制作）
 
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
